2ad2b38d67edea53a2d58db3b60e07ba8abc6903
= <=>
2021-01-06 02:11:40 +0800

    update jwt expiredIn

--- src/routes/authenticate.js
+++ src/routes/authenticate.js
@@ -32,8 +32,8 @@ async function authenticate(req, res) {
         username: user.username,
         email: user.email,
       };
-      const accessToken = await jwt.sign(payload, process.env.JWT_PRIVATE_KEY, { expiresIn: '15s' });
-      const refreshToken = await jwt.sign(payload, process.env.JWT_REFRESH_PRIVATE_KEY, { expiresIn: '20s' });
+      const accessToken = await jwt.sign(payload, process.env.JWT_PRIVATE_KEY, { expiresIn: '15m' });
+      const refreshToken = await jwt.sign(payload, process.env.JWT_REFRESH_PRIVATE_KEY, { expiresIn: '30d' });
 
       await Users.updateOne({ email }, {
         $set: {
