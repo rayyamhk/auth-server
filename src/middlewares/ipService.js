e0392ec1a5f5f35e5448a321994f56eed35e41fd
= <=>
2021-01-14 00:49:55 +0800

    test

--- src/middlewares/ipService.js
+++ src/middlewares/ipService.js
@@ -3,6 +3,7 @@ const requestIp = require('request-ip');
 function ipService() {
   return (req, res, next) => {
     req.ip = requestIp.getClientIp(req);
+    console.log(requestIp.getClientIp(req));
     next();
   };
 };
